7a79c02d96eae49198d6bf537ed9a4fee255267e
TAKI <26675318+RushnaiwalaTaki@users.noreply.github.com>
2018-12-28 21:49:08 +0530

    Add files via upload

---
+++ 404/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+	<link rel="stylesheet" type="text/css" href="/404/style/style.css">
+</head>
+<body>
+	<h1>This Is My HomePage.</h1>
+</body>
+</html>
